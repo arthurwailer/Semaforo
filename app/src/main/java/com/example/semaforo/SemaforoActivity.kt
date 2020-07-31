9ed1e8893875aa01487f31aa2eef0503fdf9dfbb
cisc.amaro@gmail.com <franFADA1993>
2020-07-31 00:11:26 -0400

    Cambio de colores Semaforos (00:11)

--- app/src/main/java/com/example/semaforo/SemaforoActivity.kt
+++ app/src/main/java/com/example/semaforo/SemaforoActivity.kt
@@ -1,27 +1,37 @@
 package com.example.semaforo
 
-import android.content.Intent
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.os.PersistableBundle
+import android.telephony.PhoneNumberFormattingTextWatcher
+import android.telephony.SmsManager
 import android.util.Log
-import android.view.View
-import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
-import com.example.semaforo.R.*
+import com.example.semaforo.R.color
+import com.example.semaforo.R.layout
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_semaforo.*
+import java.util.jar.Manifest
 
 class SemaforoActivity : AppCompatActivity() {
     internal lateinit var  db:SQLiteHelper
     private var extIdVal:Int? = null
     var contacto:ModeloContacto? = null
+    companion object {
+        const val MY_PERMISSIONS_REQUEST_SEND_SMS = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(layout.activity_semaforo)
+
+        if (ActivityCompat.checkSelfPermission(this,"android.permission.SEND_SMS")
+            != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf("android.permission.SEND_SMS"),
+                MY_PERMISSIONS_REQUEST_SEND_SMS)
+        }
 
         extIdVal = intent?.getIntExtra("ID_CONTACTO", -1)
 
@@ -34,57 +44,27 @@ class SemaforoActivity : AppCompatActivity() {
         }
         if (contacto?.mStatusRojo!!) {
             // Cambio de color de fondo a cada luz del semaforo (API 16 compatible)
-            ViewCompat.setBackgroundTintList(
-                lightRed,
-                ContextCompat.getColorStateList(
-                    getApplicationContext(),
-                    color.lightOnRed
-                )
-            )
+            ViewCompat.setBackgroundTintList(lightRed, ContextCompat.getColorStateList(
+                getApplicationContext(), color.lightOnRed))
         } else {
-            ViewCompat.setBackgroundTintList(
-                lightRed,
-                ContextCompat.getColorStateList(
-                    getApplicationContext(),
-                    color.lightOffRed
-                )
-            )
+            ViewCompat.setBackgroundTintList(lightRed, ContextCompat.getColorStateList(
+                getApplicationContext(), color.lightOffRed))
         }
         if (contacto?.mStatusNaranjo!!) {
             // Cambio de color de fondo a cada luz del semaforo (API 16 compatible)
-            ViewCompat.setBackgroundTintList(
-                lightOrange,
-                ContextCompat.getColorStateList(
-                    getApplicationContext(),
-                    color.lightOnOrange
-                )
-            )
+            ViewCompat.setBackgroundTintList(lightOrange, ContextCompat.getColorStateList(
+                getApplicationContext(), color.lightOnOrange))
         } else {
-            ViewCompat.setBackgroundTintList(
-                lightOrange,
-                ContextCompat.getColorStateList(
-                    getApplicationContext(),
-                    color.lightOffOrange
-                )
-            )
+            ViewCompat.setBackgroundTintList(lightOrange, ContextCompat.getColorStateList(
+                getApplicationContext(), color.lightOffOrange))
         }
         if (contacto?.mStatusVerde!!) {
             // Cambio de color de fondo a cada luz del semaforo (API 16 compatible)
-            ViewCompat.setBackgroundTintList(
-                lightGreen,
-                ContextCompat.getColorStateList(
-                    getApplicationContext(),
-                    color.lightOnGreen
-                )
-            )
+            ViewCompat.setBackgroundTintList(lightGreen, ContextCompat.getColorStateList(
+                getApplicationContext(), color.lightOnGreen))
         } else {
-            ViewCompat.setBackgroundTintList(
-                lightGreen,
-                ContextCompat.getColorStateList(
-                    getApplicationContext(),
-                    color.lightOffGreen
-                )
-            )
+            ViewCompat.setBackgroundTintList(lightGreen, ContextCompat.getColorStateList(
+                getApplicationContext(), color.lightOffGreen))
         }
     }
 
@@ -94,29 +74,17 @@ class SemaforoActivity : AppCompatActivity() {
         lightRed.setOnClickListener {
             if (contacto?.mStatusRojo!!) {
                 contacto?.mSmsRojoOff?.let { it1 ->
-                    Snackbar.make(view, it1, Snackbar.LENGTH_SHORT)
-                        .setAction("Action", null).show()
+                    sendSMS(it1,contacto?.mPhone!!)
                     // Cambio de color de fondo a cada luz del semaforo (API 16 compatible)
-                    ViewCompat.setBackgroundTintList(
-                        lightRed,
-                        ContextCompat.getColorStateList(
-                            getApplicationContext(),
-                            color.lightOffRed
-                        )
-                    )
+                    ViewCompat.setBackgroundTintList(lightRed, ContextCompat.getColorStateList(
+                        getApplicationContext(), color.lightOffRed))
                 }
                 contacto!!.mStatusRojo = false
             } else {
                 contacto?.mSmsRojoOn?.let { it1 ->
-                    Snackbar.make(view, it1, Snackbar.LENGTH_SHORT)
-                        .setAction("Action", null).show()
-                    ViewCompat.setBackgroundTintList(
-                        lightRed,
-                        ContextCompat.getColorStateList(
-                            getApplicationContext(),
-                            color.lightOnRed
-                        )
-                    )
+                    sendSMS(it1,contacto?.mPhone!!)
+                    ViewCompat.setBackgroundTintList(lightRed, ContextCompat.getColorStateList(
+                        getApplicationContext(), color.lightOnRed))
                 }
                 contacto!!.mStatusRojo = true
             }
@@ -126,28 +94,16 @@ class SemaforoActivity : AppCompatActivity() {
         lightOrange.setOnClickListener {
             if (contacto?.mStatusNaranjo!!) {
                 contacto?.mSmsNaranjoOff?.let { it1 ->
-                    Snackbar.make(view, it1, Snackbar.LENGTH_SHORT)
-                        .setAction("Action", null).show()
-                    ViewCompat.setBackgroundTintList(
-                        lightOrange,
-                        ContextCompat.getColorStateList(
-                            getApplicationContext(),
-                            color.lightOffOrange
-                        )
-                    )
+                    sendSMS(it1,contacto?.mPhone!!)
+                    ViewCompat.setBackgroundTintList(lightOrange, ContextCompat.getColorStateList(
+                        getApplicationContext(), color.lightOffOrange))
                 }
                 contacto!!.mStatusNaranjo = false
             } else {
                 contacto?.mSmsNaranjoOn?.let { it1 ->
-                    Snackbar.make(view, it1, Snackbar.LENGTH_SHORT)
-                        .setAction("Action", null).show()
-                    ViewCompat.setBackgroundTintList(
-                        lightOrange,
-                        ContextCompat.getColorStateList(
-                            getApplicationContext(),
-                            color.lightOnOrange
-                        )
-                    )
+                    sendSMS(it1,contacto?.mPhone!!)
+                    ViewCompat.setBackgroundTintList(lightOrange, ContextCompat.getColorStateList(
+                        getApplicationContext(), color.lightOnOrange))
                 }
                 contacto!!.mStatusNaranjo = true
             }
@@ -157,29 +113,16 @@ class SemaforoActivity : AppCompatActivity() {
         lightGreen.setOnClickListener {
             if (contacto?.mStatusVerde!!) {
                 contacto?.mSmsVerdeOff?.let { it1 ->
-                    Snackbar.make(view, it1, Snackbar.LENGTH_SHORT)
-                        .setAction("Action", null).show()
-
-                    ViewCompat.setBackgroundTintList(
-                        lightGreen,
-                        ContextCompat.getColorStateList(
-                            getApplicationContext(),
-                            color.lightOffGreen
-                        )
-                    )
+                    sendSMS(it1,contacto?.mPhone!!)
+                    ViewCompat.setBackgroundTintList(lightGreen, ContextCompat.getColorStateList(
+                        getApplicationContext(), color.lightOffGreen))
                 }
                 contacto!!.mStatusVerde = false
             } else {
                 contacto?.mSmsVerdeOn?.let { it1 ->
-                    Snackbar.make(view, it1, Snackbar.LENGTH_SHORT)
-                        .setAction("Action", null).show()
-                    ViewCompat.setBackgroundTintList(
-                        lightGreen,
-                        ContextCompat.getColorStateList(
-                            getApplicationContext(),
-                            color.lightOnGreen
-                        )
-                    )
+                    sendSMS(it1,contacto?.mPhone!!)
+                    ViewCompat.setBackgroundTintList(lightGreen, ContextCompat.getColorStateList(
+                        getApplicationContext(), color.lightOnGreen))
                 }
                 contacto!!.mStatusVerde = true
             }
@@ -187,4 +130,17 @@ class SemaforoActivity : AppCompatActivity() {
         }
     }
 
+    private fun sendSMS(msg:String,phone:Int) {
+        try {
+            val sms = SmsManager.getDefault()
+            sms.sendTextMessage("+569"+phone,null,
+                msg,null,null)
+            Snackbar.make(view, "SMS ENVIADO: "+msg, Snackbar.LENGTH_SHORT)
+                .setAction("Action", null).show()
+        } catch (e: Exception) {
+            Snackbar.make(view, "No Pudo Enviar SMS", Snackbar.LENGTH_SHORT)
+                .setAction("Action", null).show()
+            e.printStackTrace()
+        }
+    }
 }
